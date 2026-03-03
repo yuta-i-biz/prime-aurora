ca36624c9c49f28005a422984f2c9d5ddfd82d6b
今川雄太 <imagawayuuta@imagawayuutanoMacBook-Air.local>
2026-03-03 10:27:09 +0900

    feat: implement task calendar, time support, responsive, and completed filters

--- src/app/calendar/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getTasks } from "@/app/actions/tasks";
-import CalendarClient from "./CalendarClient";
-
-export default async function CalendarPage() {
-    const tasks = await getTasks();
-    return <CalendarClient initialTasks={tasks} />;
-}
